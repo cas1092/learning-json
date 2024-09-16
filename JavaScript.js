2a14adcde9ed1c13a472478508137c7362a30bc2
cas1092 <castillo.goche.l@gmail.com>
2024-09-16 14:48:45 -0500

    Initial commit

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+﻿async function populate() {
+  const requestURL = "";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const superHeroes = await response.json();
+
+  populateHeader(superHeroes);
+  populateHeroes(superHeroes);
+}
